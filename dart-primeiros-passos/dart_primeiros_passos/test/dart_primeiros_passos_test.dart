99602bf19ef573118e27f187750986b53765afd8
Caroline Vicente <cvicente@magnasistemas.com.br>
2023-04-24 17:48:18 -0300

    cursos dart alura

---
+++ dart-primeiros-passos/dart_primeiros_passos/test/dart_primeiros_passos_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_primeiros_passos/dart_primeiros_passos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
